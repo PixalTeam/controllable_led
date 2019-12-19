50cca9db0795b12aebdd2d3b9ebcd3c6bf0da953
Kellis <vikiclashonyt@gmail.com>
2019-12-19 09:44:19 +0100

    final version

---
+++ README.md
@@ -0,0 +1,36 @@
+# Instagram-API-python
+
+This C++ Arduino Software works with the Android App available here <a href="https://github.com/PixalTeam/controllable_led_apk">https://github.com/PixalTeam/controllable_led_apk</a>.
+
+## Use
+
+1. Clone/Download these gits
+
+    `git clone https://github.com/PixalTeam/controllable_led`
+    `git clone https://github.com/PixalTeam/controllable_led_apk`
+
+2. Open the .ino file and install the .apk on your Android phone
+
+3. Upload the .ino into your Arduino connected with a Bluetooth connector to TX and RX
+
+4. Plug all DELs in
+
+<img src="https://www.mediafire.com/convkey/416f/rex08z0y3qlr6vtzg.jpg" width="100%">
+
+5. Connect your Arduino to your Phone by Bluetooth and enjoy
+
+## Donate
+
+If you want to help me in my work, consider donating. Thank you!
+
+### ![Paypal](https://raw.githubusercontent.com/reek/anti-adblock-killer/gh-pages/images/paypal.png) Paypal
+
+- [https://www.paypal.me/Gumbraise](https://www.paypal.me/Gumbraise)
+
+### ![btc](https://raw.githubusercontent.com/reek/anti-adblock-killer/gh-pages/images/bitcoin.png) Bitcoin
+
+- 3ML4B58aw9CHTsjE5nPGzVhTLK8mBz8Vv4
+
+### <img src="https://www.logolynx.com/images/logolynx/b0/b0839301e62a21664ea82d24ab1a0414.png" width="16" height="16" /> Ethereum
+
+- 0xf1fee16e2b91b462139434b9c5FE949856ccc6eb
--- sketch_oct29a.ino
+++ sketch_oct29a.ino
@@ -1,43 +1,49 @@
 int red = 7;
 int yellow = 8;
 int green = 9;
-int reponser;
-int reponsey;
-int reponseg;
-char which;
- 
+
+int brightness = 0;
+String reponse;
+  
 void setup()
 {
   pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
   pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
   Serial.begin(9600);
 }
-    
+     
 enum EtatCourant {
    AttenteReponse,
 }
-  
-etat = AttenteReponse;
- 
- 
- 
-          
+   
+etat = AttenteReponse;  
+           
 void loop()
 {
   
           if (Serial.available() > 0) {
-            std::string s = Serial.read();
-            std::string delimiter = ":";
-            std::string token = s.substr(0, s.find(delimiter));
-            if (token == 'R') {
-              Serial.println("RED");
+            reponse = Serial.readString();
+ 
+            if(reponse.substring(0,1)=="R")
+            {
+              analogWrite(red, reponse.substring(1).toInt());
+              Serial.println(reponse.substring(1));
             }
-            if (token == 'G') {
-              Serial.println("GREEN");
+
+            if(reponse.substring(0,1)=="G")
+            {
+              analogWrite(green, reponse.substring(1).toInt());
+              Serial.println(reponse.substring(1));
             }
-            /*reponse = Serial.readString().toInt();
-            Serial.println(reponse, DEC);
-            analogWrite(green, reponse);*/
-          }
+             
+            if(reponse.substring(0,1)=="Y")
+            {
+              analogWrite(yelow, reponse.substring(1).toInt());
+              Serial.println(reponse.substring(1));
+            }
+             
+              
+            }
+             
 }
